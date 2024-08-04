865f33c17d63132961d38e10148e19989d40b568
tanyaagupta <120319976+tanyaagupta@users.noreply.github.com>
2024-08-05 03:27:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import {getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCHRC9Nijxokkj3KPWQxpHvF31xGELFcZ0",
+  apiKey: "abcFcZ0",
   authDomain: "inventorymanagement-6b4eb.firebaseapp.com",
   projectId: "inventorymanagement-6b4eb",
   storageBucket: "inventorymanagement-6b4eb.appspot.com",
   messagingSenderId: "1001756721364",
-  appId: "1:1001756721364:web:b893c5242c2f513d693b41",
+  appId: "defb893c52421",
   measurementId: "G-TMRJ8Z7XPJ"
 };
 
